8097a8ce51fd6f9b0ee1def2662e47497fdc4831
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-09 06:18:31 +0700

    export color

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+/// special imports
 import './a_index';
+///
+
 import { buildPost } from './buildPost';
 import { generatePostId } from './generatePostId';
 import { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
 import { parsePermalink } from './parsePermalink';
 import { parsePost } from './parsePost';
+export { generatePostId } from './generatePostId';
+export { DeepPartial, Nullable } from './globals';
+export { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
+export * as color from './node/color';
+export { parsePermalink } from './parsePermalink';
+export { ParseOptions, postAuthor, postMap, postMeta } from './types';
+export { parsePost, buildPost };
 
 const hexoPostParser = {
   parsePost,
@@ -14,12 +24,3 @@ const hexoPostParser = {
 };
 
 export default hexoPostParser;
-
-export { generatePostId } from './generatePostId';
-export { DeepPartial, Nullable } from './globals';
-export { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
-export { parsePermalink } from './parsePermalink';
-export { ParseOptions, postAuthor, postMap, postMeta } from './types';
-export { parsePost, buildPost };
-
-//
