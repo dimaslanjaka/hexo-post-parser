a710ec2086e7dcb281f8237777f1f1690c475654
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-09 07:24:17 +0700

    build Mon Jan 09 2023 07:24:17 GMT+0700 (Western Indonesia Time)

--- dist/types/index.js
+++ dist/types/index.js
@@ -1,3 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.setConfig = exports.getConfig = void 0;
+var _config_1 = require("./_config");
+Object.defineProperty(exports, "getConfig", { enumerable: true, get: function () { return _config_1.getConfig; } });
+Object.defineProperty(exports, "setConfig", { enumerable: true, get: function () { return _config_1.setConfig; } });
 //# sourceMappingURL=index.js.map
