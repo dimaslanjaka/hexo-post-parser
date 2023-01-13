30bde32e105fcb799819d58c4f922168fa96b6af
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-14 01:58:04 +0700

    remove failure builds

--- dist/generatePostId.d.ts
+++
@@ -1,7 +0,0 @@
-import { postMeta } from './types/postMeta';
-/**
- * generate post id using uuid v4
- * @param meta
- * @returns
- */
-export declare function generatePostId(meta: postMeta | string): string;
