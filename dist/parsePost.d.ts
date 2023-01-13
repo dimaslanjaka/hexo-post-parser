1fb3a7040810f0d24e188b2ee2ec4f7f1e66f2fd
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-14 01:57:11 +0700

    build Sat Jan 14 2023 01:57:11 GMT+0700 (Western Indonesia Time)

--- dist/parsePost.d.ts
+++ dist/parsePost.d.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ParseOptions, postMap } from './types';
 /**
  * Parse Hexo markdown post (structured with yaml and universal markdown blocks)
@@ -10,17 +9,3 @@ import { ParseOptions, postMap } from './types';
  */
 export declare function parsePost(target: string, options?: ParseOptions): Promise<postMap>;
 export default parsePost;
-=======
-import { ParseOptions, postMap } from './types';
-/**
- * Parse Hexo markdown post (structured with yaml and universal markdown blocks)
- * * return {@link postMap} metadata {string & object} and body
- * * return {@link null} == failed
- * @param target file path or string markdown contents (used for cache key)
- * @param options options parser
- * * {@link ParseOptions.sourceFile} used for cache key when `target` is file contents
- */
-export declare function parsePost(target: string, options?: ParseOptions): Promise<postMap>;
-export default parsePost;
-export declare function parseFrontMatter(): void;
->>>>>>> 69894b83be89220f9db6f81cafe83ae8fe8f2882
