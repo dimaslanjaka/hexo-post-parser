42837f22d02674f7114d989d0ea694edbef4b5ea
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-12 05:05:38 +0700

    feat: add jest feat: separate exports

---
+++ src/index-exports.ts
@@ -0,0 +1,17 @@
+/// special imports
+import './a_index';
+///
+
+/// exports
+export * from './buildPost';
+export * from './generatePostId';
+export * from './globals';
+export { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
+export * as color from './node/color';
+export * from './parsePermalink';
+export * from './parsePost';
+export * from './parsePost-front-matter';
+export * from './types';
+export { getConfig, setConfig } from './types/_config';
+
+////
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-/// special imports
-import './a_index';
-///
+export * from './index-exports';
+import * as exportt from './index-exports';
 
-/// exports
-export * from './buildPost';
-export * from './generatePostId';
-export * from './globals';
-export { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
-export * as color from './node/color';
-export * from './parsePermalink';
-export * from './parsePost';
-export * from './parsePost-front-matter';
-export * from './types';
-export { getConfig, setConfig } from './types/_config';
-
-////
+export default exportt;
