1a1f1cca906110521ee9f7878bba78cbcd5a74e3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-12 05:08:56 +0700

    apply hotfix to master

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-/// special imports
-import './a_index';
-///
+export * from './index-exports';
+import * as exportt from './index-exports';
 
-/// exports
-export { buildPost } from './buildPost';
-export { generatePostId } from './generatePostId';
-export { DeepPartial, Nullable } from './globals';
-export { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
-export * as color from './node/color';
-export { parsePermalink } from './parsePermalink';
-export { parsePost } from './parsePost';
-export { ParseOptions, postAuthor, postMap, postMeta } from './types';
-export { getConfig, setConfig } from './types/_config';
-
-////
+export default exportt;
