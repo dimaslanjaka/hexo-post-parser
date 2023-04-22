1cca7000aee35207639211aec57bbc5aca371fb8
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-22 18:48:59 +0700

    chore: update dependencies

---
+++ dist/node/slugify/replacements.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const overridableReplacements = [
+    ['&', ' and '],
+    ['🦄', ' unicorn '],
+    ['♥', ' love '],
+];
+exports.default = overridableReplacements;
