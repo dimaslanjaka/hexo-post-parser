981c0f6b78979bf15ac849c8503b11dcce40d66a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-11 21:13:18 +0700

    refactor: fixed date test

--- test/dates.ts
+++ test/dates.ts
@@ -9,12 +9,13 @@ import { join, readDirectoryRecursive } from '../src/node/filemanager';
 import './config';
 import { startParse } from './startParse';
 
-const files = readDirectoryRecursive(join(__dirname, 'src-posts/date'));
-files.then((files) => {
+(async () => {
+  const files = await readDirectoryRecursive(join(__dirname, 'src-posts/date'));
   if (Array.isArray(files)) {
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
-      startParse(file, getConfig());
+      const post = await startParse(file, getConfig());
+      if (post) console.log(post);
     }
   }
-});
+})();
