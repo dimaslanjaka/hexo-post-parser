dae86f1c7024a7130dafb4fdd1e70d195d02a1e9
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-19 14:57:10 +0700

    drop gulp task

--- src/cleaner.ts
+++ src/cleaner.ts
@@ -1,4 +1,3 @@
-import gulp from 'gulp';
 import { scheduler } from 'sbg-utility';
 import { TaskCallback } from 'undertaker';
 import { dbFolder } from './node/cache';
@@ -11,28 +10,18 @@ scheduler.register();
 const config = () => getConfig();
 
 /** clean generated folder */
-const clean_public = (done?: TaskCallback) =>
+export const clean_public = (done?: TaskCallback) =>
   rm(join(config().root, config().public_dir), { recursive: true }, done);
 /** clean posts from config.source_dir */
-const clean_posts = (done?: TaskCallback) =>
+export const clean_posts = (done?: TaskCallback) =>
   rm(
     join(config().root, config().source_dir, '_posts'),
     { recursive: true },
     done
   );
 /** clean temp folder */
-const clean_tmp = (done?: TaskCallback) =>
+export const clean_tmp = (done?: TaskCallback) =>
   rm(join(config().root, 'tmp'), { recursive: true }, done);
 /** clean database folder */
-const clean_db = (done?: TaskCallback) =>
+export const clean_db = (done?: TaskCallback) =>
   rm(dbFolder, { recursive: true }, done);
-
-gulp.task('clean:public', clean_public);
-gulp.task('clean:posts', clean_posts);
-gulp.task('clean:db', clean_db);
-gulp.task('clean:tmp', clean_tmp);
-gulp.task(
-  'clean',
-  gulp.parallel('clean:db', 'clean:tmp', 'clean:posts', 'clean:public')
-);
-gulp.task('default', gulp.series('copy', 'generate'));
