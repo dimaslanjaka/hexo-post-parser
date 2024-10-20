a140b3c4a351c45b0a4d1e22917be0977c18997f
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-21 02:52:19 +0700

    update local deps

--- package-switch.cjs
+++ package-switch.cjs
@@ -13,11 +13,19 @@ const local = {
   'sbg-cli':
     'file:../static-blog-generator/packages/sbg-cli/release/sbg-cli.tgz',
   'hexo-asset-link': 'file:../hexo/releases/hexo-asset-link.tgz',
+  hexo: 'file:../hexo/releases/hexo.tgz',
   'hexo-cli': 'file:../hexo/releases/hexo-cli.tgz',
   'hexo-front-matter': 'file:../hexo/releases/hexo-front-matter.tgz',
   'hexo-log': 'file:../hexo/releases/hexo-log.tgz',
   'hexo-util': 'file:../hexo/releases/hexo-util.tgz',
-  warehouse: 'file:../hexo/releases/warehouse.tgz'
+  warehouse: 'file:../hexo/releases/warehouse.tgz',
+  'hexo-post-parser': 'file:../hexo-post-parser/release/hexo-post-parser.tgz',
+  'git-command-helper':
+    'file:../git-command-helper/release/git-command-helper.tgz',
+  'markdown-it': 'file:../markdown-it/release/markdown-it.tgz',
+  'hexo-renderers': 'file:../hexo-renderers/release/hexo-renderers.tgz',
+  'hexo-shortcodes': 'file:../hexo-shortcodes/release/hexo-shortcodes.tgz',
+  'hexo-seo': 'file:../hexo-seo/release/hexo-seo.tgz'
 };
 
 // node package-switch.js [local|production]
