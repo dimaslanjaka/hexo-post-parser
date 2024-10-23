8c4e98159e4407b77e1c25e028c1fd4a0073a8e3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2024-10-23 19:41:33 +0700

    Update package-switch.cjs

--- package-switch.cjs
+++ package-switch.cjs
@@ -6,26 +6,24 @@ const path = require('path');
 
 const local = {
   'sbg-api': 'file:../sbg-api/packages/sbg-api/release/sbg-api.tgz',
-  'sbg-utility':
-    'file:../sbg-utility/packages/sbg-utility/release/sbg-utility.tgz',
-  'sbg-server':
-    'file:../static-blog-generator/packages/sbg-server/release/sbg-server.tgz',
-  'sbg-cli':
-    'file:../static-blog-generator/packages/sbg-cli/release/sbg-cli.tgz',
+  'sbg-utility': 'file:../sbg-utility/packages/sbg-utility/release/sbg-utility.tgz',
+  'sbg-server': 'file:../static-blog-generator/packages/sbg-server/release/sbg-server.tgz',
+  'sbg-cli': 'file:../static-blog-generator/packages/sbg-cli/release/sbg-cli.tgz',
   'hexo-asset-link': 'file:../hexo/releases/hexo-asset-link.tgz',
   hexo: 'file:../hexo/releases/hexo.tgz',
   'hexo-cli': 'file:../hexo/releases/hexo-cli.tgz',
+  'hexo-server': 'file:../hexo/releases/hexo-server.tgz',
   'hexo-front-matter': 'file:../hexo/releases/hexo-front-matter.tgz',
   'hexo-log': 'file:../hexo/releases/hexo-log.tgz',
   'hexo-util': 'file:../hexo/releases/hexo-util.tgz',
   warehouse: 'file:../hexo/releases/warehouse.tgz',
   'hexo-post-parser': 'file:../hexo-post-parser/release/hexo-post-parser.tgz',
-  'git-command-helper':
-    'file:../git-command-helper/release/git-command-helper.tgz',
+  'git-command-helper': 'file:../git-command-helper/release/git-command-helper.tgz',
   'markdown-it': 'file:../markdown-it/release/markdown-it.tgz',
   'hexo-renderers': 'file:../hexo-renderers/release/hexo-renderers.tgz',
   'hexo-shortcodes': 'file:../hexo-shortcodes/release/hexo-shortcodes.tgz',
-  'hexo-seo': 'file:../hexo-seo/release/hexo-seo.tgz'
+  'hexo-seo': 'file:../hexo-seo/release/hexo-seo.tgz',
+  'google-news-sitemap': 'file:../google-news-sitemap/release/google-news-sitemap.tgz'
 };
 
 // node package-switch.js [local|production]
